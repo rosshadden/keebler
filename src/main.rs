b80db25c0e3f0fe600fc86c3d5f9a3bff414c4b7
Ross Hadden <rosshadden@gmail.com>
2024-01-26 15:47:44 -0500

    feat: working skeleton

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use defmt::*;
+use embassy_executor::Spawner;
+use embassy_rp::{bind_interrupts, pio::InterruptHandler, peripherals::PIO0};
+use {defmt_rtt as _, panic_probe as _};
+
+bind_interrupts!(struct Irqs {
+	PIO0_IRQ_0 => InterruptHandler<PIO0>;
+});
+
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
+	info!("hi there");
 }
