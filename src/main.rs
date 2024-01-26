791596c19ee4594e27a071ded6f69591ee3543ab
Ross Hadden <rosshadden@gmail.com>
2024-01-26 16:47:57 -0500

    feat: keyboard hid example working

--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,190 @@
 #![no_std]
 #![no_main]
 
-use cyw43_pio::PioSpi;
+use core::sync::atomic::{AtomicBool, Ordering};
+
 use defmt::*;
 use embassy_executor::Spawner;
+use embassy_futures::join::join;
 use embassy_rp::bind_interrupts;
-use embassy_rp::gpio::{Level, Output};
-use embassy_rp::peripherals::{DMA_CH0, PIO0};
-use embassy_rp::pio::{InterruptHandler, Pio};
-use embassy_time::{Duration, Timer};
-use static_cell::StaticCell;
+use embassy_rp::gpio::{Input, Pull};
+use embassy_rp::peripherals::USB;
+use embassy_rp::usb::{Driver, InterruptHandler};
+use embassy_usb::class::hid::{HidReaderWriter, ReportId, RequestHandler, State};
+use embassy_usb::control::OutResponse;
+use embassy_usb::{Builder, Config, Handler};
+use usbd_hid::descriptor::{KeyboardReport, SerializedDescriptor};
 use {defmt_rtt as _, panic_probe as _};
 
 bind_interrupts!(struct Irqs {
-	PIO0_IRQ_0 => InterruptHandler<PIO0>;
+	USBCTRL_IRQ => InterruptHandler<USB>;
 });
 
-#[embassy_executor::task]
-async fn wifi_task(runner: cyw43::Runner<'static, Output<'static, embassy_rp::peripherals::PIN_23>, PioSpi<'static, embassy_rp::peripherals::PIN_25, PIO0, 0, DMA_CH0>>) -> ! {
-// async fn wifi_task(runner: cyw43::Runner<'static, Output<'static>, PioSpi<'static, PIO0, 0, DMA_CH0>>) -> ! {
-	runner.run().await
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
+	let p = embassy_rp::init(Default::default());
+	// Create the driver, from the HAL.
+	let driver = Driver::new(p.USB, Irqs);
+
+	// Create embassy-usb Config
+	let mut config = Config::new(0xc0de, 0xcafe);
+	config.manufacturer = Some("Embassy");
+	config.product = Some("HID keyboard example");
+	config.serial_number = Some("12345678");
+	config.max_power = 100;
+	config.max_packet_size_0 = 64;
+
+	// Create embassy-usb DeviceBuilder using the driver and config.
+	// It needs some buffers for building the descriptors.
+	let mut device_descriptor = [0; 256];
+	let mut config_descriptor = [0; 256];
+	let mut bos_descriptor = [0; 256];
+	// You can also add a Microsoft OS descriptor.
+	let mut msos_descriptor = [0; 256];
+	let mut control_buf = [0; 64];
+	let request_handler = MyRequestHandler {};
+	let mut device_handler = MyDeviceHandler::new();
+
+	let mut state = State::new();
+
+	let mut builder = Builder::new(
+		driver,
+		config,
+		&mut device_descriptor,
+		&mut config_descriptor,
+		&mut bos_descriptor,
+		&mut msos_descriptor,
+		&mut control_buf,
+	);
+
+	builder.handler(&mut device_handler);
+
+	// Create classes on the builder.
+	let config = embassy_usb::class::hid::Config {
+		report_descriptor: KeyboardReport::desc(),
+		request_handler: Some(&request_handler),
+		poll_ms: 60,
+		max_packet_size: 64,
+	};
+	let hid = HidReaderWriter::<_, 1, 8>::new(&mut builder, &mut state, config);
+
+	// Build the builder.
+	let mut usb = builder.build();
+
+	// Run the USB device.
+	let usb_fut = usb.run();
+
+	// Set up the signal pin that will be used to trigger the keyboard.
+	let mut signal_pin = Input::new(p.PIN_16, Pull::None);
+
+	// Enable the schmitt trigger to slightly debounce.
+	signal_pin.set_schmitt(true);
+
+	let (reader, mut writer) = hid.split();
+
+	// Do stuff with the class!
+	let in_fut = async {
+		loop {
+			info!("Waiting for HIGH on pin 16");
+			signal_pin.wait_for_high().await;
+			info!("HIGH DETECTED");
+			// Create a report with the A key pressed. (no shift modifier)
+			let report = KeyboardReport {
+				keycodes: [4, 0, 0, 0, 0, 0],
+				leds: 0,
+				modifier: 0,
+				reserved: 0,
+			};
+			// Send the report.
+			match writer.write_serialize(&report).await {
+				Ok(()) => {}
+				Err(e) => warn!("Failed to send report: {:?}", e),
+			};
+			signal_pin.wait_for_low().await;
+			info!("LOW DETECTED");
+			let report = KeyboardReport {
+				keycodes: [0, 0, 0, 0, 0, 0],
+				leds: 0,
+				modifier: 0,
+				reserved: 0,
+			};
+			match writer.write_serialize(&report).await {
+				Ok(()) => {}
+				Err(e) => warn!("Failed to send report: {:?}", e),
+			};
+		}
+	};
+
+	let out_fut = async {
+		reader.run(false, &request_handler).await;
+	};
+
+	// Run everything concurrently.
+	// If we had made everything `'static` above instead, we could do this using separate tasks instead.
+	join(usb_fut, join(in_fut, out_fut)).await;
 }
 
-#[embassy_executor::main]
-async fn main(spawner: Spawner) {
-	let p = embassy_rp::init(Default::default());
+struct MyRequestHandler {}
 
-	// let fw = include_bytes!("../lib/fw/43439A0.bin");
-	// let clm = include_bytes!("../lib/fw/43439A0_clm.bin");
-	let fw = unsafe { core::slice::from_raw_parts(0x10100000 as *const u8, 230321) };
-	let clm = unsafe { core::slice::from_raw_parts(0x10140000 as *const u8, 4752) };
+impl RequestHandler for MyRequestHandler {
+	fn get_report(&self, id: ReportId, _buf: &mut [u8]) -> Option<usize> {
+		info!("Get report for {:?}", id);
+		None
+	}
 
-	let pwr = Output::new(p.PIN_23, Level::Low);
-	let cs = Output::new(p.PIN_25, Level::High);
-	let mut pio = Pio::new(p.PIO0, Irqs);
-	let spi = PioSpi::new(&mut pio.common, pio.sm0, pio.irq0, cs, p.PIN_24, p.PIN_29, p.DMA_CH0);
+	fn set_report(&self, id: ReportId, data: &[u8]) -> OutResponse {
+		info!("Set report for {:?}: {=[u8]}", id, data);
+		OutResponse::Accepted
+	}
 
-	static STATE: StaticCell<cyw43::State> = StaticCell::new();
-	let state = STATE.init(cyw43::State::new());
-	let (_net_device, mut control, runner) = cyw43::new(state, pwr, spi, fw).await;
-	unwrap!(spawner.spawn(wifi_task(runner)));
+	fn set_idle_ms(&self, id: Option<ReportId>, dur: u32) {
+		info!("Set idle rate for {:?} to {:?}", id, dur);
+	}
 
-	control.init(clm).await;
-	control.set_power_management(cyw43::PowerManagementMode::PowerSave).await;
+	fn get_idle_ms(&self, id: Option<ReportId>) -> Option<u32> {
+		info!("Get idle rate for {:?}", id);
+		None
+	}
+}
 
-	let delay = Duration::from_secs(1);
-	loop {
-		info!("led on!");
-		control.gpio_set(0, true).await;
-		Timer::after(delay).await;
+struct MyDeviceHandler {
+	configured: AtomicBool,
+}
 
-		info!("led off!");
-		control.gpio_set(0, false).await;
-		Timer::after(delay).await;
+impl MyDeviceHandler {
+	fn new() -> Self {
+		MyDeviceHandler {
+			configured: AtomicBool::new(false),
+		}
+	}
+}
+
+impl Handler for MyDeviceHandler {
+	fn enabled(&mut self, enabled: bool) {
+		self.configured.store(false, Ordering::Relaxed);
+		if enabled {
+			info!("Device enabled");
+		} else {
+			info!("Device disabled");
+		}
+	}
+
+	fn reset(&mut self) {
+		self.configured.store(false, Ordering::Relaxed);
+		info!("Bus reset, the Vbus current limit is 100mA");
+	}
+
+	fn addressed(&mut self, addr: u8) {
+		self.configured.store(false, Ordering::Relaxed);
+		info!("USB address set to: {}", addr);
+	}
+
+	fn configured(&mut self, configured: bool) {
+		self.configured.store(configured, Ordering::Relaxed);
+		if configured {
+			info!("Device configured, it may now draw up to the configured current limit from Vbus.")
+		} else {
+			info!("Device is no longer configured, the Vbus current limit is 100mA.");
+		}
 	}
 }
