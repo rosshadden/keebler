a4b2f0cb812e527dafd905e1d2507e4b3e5b9491
Ross Hadden <rosshadden@gmail.com>
2024-01-30 17:10:56 -0500

    feat: working hid keyboard

--- build.zig
+++ build.zig
@@ -4,64 +4,28 @@ const rp2040 = @import("rp2040");
 pub fn build(b: *std.Build) void {
     const microzig = @import("microzig").init(b, "microzig");
     const optimize = b.standardOptimizeOption(.{});
-    const target = b.standardTargetOptions(.{});
+
+    const hana = b.createModule(.{ .source_file = .{ .path = "./lib/kirei/src/lib/hana/hana.zig" } });
+    const umm = b.createModule(.{ .source_file = .{ .path = "./lib/kirei/src/lib/umm/umm.zig" } });
+    const uuid = b.createModule(.{ .source_file = .{ .path = "./lib/kirei/src/lib/uuid/uuid.zig" } });
+
+    const kirei = b.createModule(.{
+        .source_file = .{ .path = "./lib/kirei/src/kirei/engine.zig" },
+        .dependencies = &.{
+            .{ .name = "hana", .module = hana },
+        },
+    });
+
+    const platform = b.option(
+        enum { local, rp2040 },
+        "platform",
+        "Platform to build for",
+    ) orelse .local;
+
+    const target = switch (platform) {
+        else => b.standardTargetOptions(.{}),
+    };
     _ = target;
-
-    // const exe = b.addExecutable(.{
-    //     .name = "keebler",
-    //     // In this case the main source file is merely a path, however, in more
-    //     // complicated build scripts, this could be a generated file.
-    //     .root_source_file = .{ .path = "src/main.zig" },
-    //     .target = target,
-    //     .optimize = optimize,
-    // });
-    //
-    // // This declares intent for the executable to be installed into the
-    // // standard location when the user invokes the "install" step (the default
-    // // step when running `zig build`).
-    // b.installArtifact(exe);
-    //
-    // // This *creates* a Run step in the build graph, to be executed when another
-    // // step is evaluated that depends on it. The next line below will establish
-    // // such a dependency.
-    // const run_cmd = b.addRunArtifact(exe);
-    //
-    // // By making the run step depend on the install step, it will be run from the
-    // // installation directory rather than directly from within the cache directory.
-    // // This is not necessary, however, if the application depends on other installed
-    // // files, this ensures they will be present and in the expected location.
-    // run_cmd.step.dependOn(b.getInstallStep());
-    //
-    // // This allows the user to pass arguments to the application in the build
-    // // command itself, like this: `zig build run -- arg1 arg2 etc`
-    // if (b.args) |args| {
-    //     run_cmd.addArgs(args);
-    // }
-    //
-    // // This creates a build step. It will be visible in the `zig build --help` menu,
-    // // and can be selected like this: `zig build run`
-    // // This will evaluate the `run` step rather than the default, which is "install".
-    // const run_step = b.step("run", "Run the app");
-    // run_step.dependOn(&run_cmd.step);
-    //
-    // // Creates a step for unit testing. This only builds the test executable
-    // // but does not run it.
-    // const unit_tests = b.addTest(.{
-    //     .root_source_file = .{ .path = "src/main.zig" },
-    //     .target = target,
-    //     .optimize = optimize,
-    // });
-    //
-    // const run_unit_tests = b.addRunArtifact(unit_tests);
-    //
-    // // Similar to creating the run step earlier, this exposes a `test` step to
-    // // the `zig build --help` menu, providing a way for the user to request
-    // // running the unit tests.
-    // const test_step = b.step("test", "Run unit tests");
-    // test_step.dependOn(&run_unit_tests.step);
-
-    //
-    //
 
     const firmware = microzig.addFirmware(b, .{
         .name = "keebler",
@@ -70,12 +34,11 @@ pub fn build(b: *std.Build) void {
         .source_file = .{ .path = "src/main.zig" },
     });
 
-    // `installFirmware()` is the MicroZig pendant to `Build.installArtifact()`
-    // and allows installing the firmware as a typical firmware file.
-    //
-    // This will also install into `$prefix/firmware` instead of `$prefix/bin`.
-    microzig.installFirmware(b, firmware, .{});
+    firmware.addAppDependency("hana", hana, .{});
+    firmware.addAppDependency("kirei", kirei, .{});
+    firmware.addAppDependency("umm", umm, .{});
+    firmware.addAppDependency("uuid", uuid, .{});
 
-    // For debugging, we also always install the firmware as an ELF file
+    microzig.installFirmware(b, firmware, .{});
     microzig.installFirmware(b, firmware, .{ .format = .elf });
 }
