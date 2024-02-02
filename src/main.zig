e5bc4a7f5ca2e2deec5d409322ae76f27d349832
Ross Hadden <rosshadden@gmail.com>
2024-02-02 16:13:10 -0500

    feat: implemented keyboard matrix for great good

--- src/main.zig
+++ src/main.zig
@@ -8,8 +8,11 @@ const rp2040 = microzig.hal;
 const time = rp2040.time;
 const gpio = rp2040.gpio;
 
-const key1 = gpio.num(14);
-const key2 = gpio.num(15);
+const col0 = gpio.num(14);
+const col1 = gpio.num(15);
+const row0 = gpio.num(16);
+const row1 = gpio.num(17);
+
 const uart = rp2040.uart.num(0);
 
 pub const std_options = struct {
@@ -24,13 +27,17 @@ pub fn panic(message: []const u8, _: ?*std.builtin.StackTrace, _: ?usize) noretu
 }
 
 pub fn main() !void {
-    key1.set_function(.sio);
-    key1.set_pull(.up);
-    key1.set_direction(.in);
+    row0.set_function(.sio);
+    row0.set_pull(.up);
+    row0.set_direction(.in);
+    row1.set_function(.sio);
+    row1.set_pull(.up);
+    row1.set_direction(.in);
 
-    key2.set_function(.sio);
-    key2.set_pull(.up);
-    key2.set_direction(.in);
+    col0.set_function(.sio);
+    col0.set_direction(.out);
+    col1.set_function(.sio);
+    col1.set_direction(.out);
 
     uart.apply(.{
         .baud_rate = 115_200,
@@ -44,29 +51,47 @@ pub fn main() !void {
     try usb.init();
     interface.init();
 
-    var last_key1: bool = false;
-    var last_key2: bool = false;
+    var last_a = false;
+    var last_b = false;
+    var last_c = false;
+    var last_d = false;
 
     while (true) {
         try usb.process();
         interface.process();
 
-        if (!last_key1 and key1.read() == 0) {
+        col0.put(0);
+        if (!last_a and row0.read() == 0) {
             interface.pushKeyEvent(0, true);
-            last_key1 = true;
-        } else if (last_key1 and key1.read() == 1) {
+            last_a = !last_a;
+        } else if (last_a and row0.read() == 1) {
             interface.pushKeyEvent(0, false);
-            last_key1 = false;
+            last_a = !last_a;
         }
+        if (!last_c and row1.read() == 0) {
+            interface.pushKeyEvent(2, true);
+            last_c = !last_c;
+        } else if (last_c and row1.read() == 1) {
+            interface.pushKeyEvent(2, false);
+            last_c = !last_c;
+        }
+        col0.put(1);
 
-        if (!last_key2 and key2.read() == 0) {
+        col1.put(0);
+        if (!last_b and row0.read() == 0) {
             interface.pushKeyEvent(1, true);
-            last_key2 = true;
-        } else if (last_key2 and key2.read() == 1) {
+            last_b = !last_b;
+        } else if (last_b and row0.read() == 1) {
             interface.pushKeyEvent(1, false);
-            last_key2 = false;
+            last_b = !last_b;
         }
-
-        time.sleep_ms(50);
+        if (!last_d and row1.read() == 0) {
+            interface.pushKeyEvent(3, true);
+            last_d = !last_d;
+        } else if (last_d and row1.read() == 1) {
+            interface.pushKeyEvent(3, false);
+            last_d = !last_d;
+        }
+        col1.put(1);
     }
 }
